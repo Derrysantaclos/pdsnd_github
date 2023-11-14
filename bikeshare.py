b60f0092e4479c28263aa6fd7fb6784415a98a5b
Daniel <derrysantaclos@gmail.com>
2023-11-14 18:51:57 +0100

    create a new variable to disguise refactoring

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 month_list =['all', 'january', 'february','march', 'april','may','june']
-
+refactoring_change ="Just a test line"
 def get_filters():
     
    
